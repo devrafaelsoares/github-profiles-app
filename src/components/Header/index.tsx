67c5cfc447ccdb20f05e5c0757f71a609ed14481
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-02-02 11:37:11 -0300

    feat: add application header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import Logo from '@/components/icons/Logo';
+import ThemeModeToogle from '@/components/ThemeModeToogle';
+import { author } from '@/data/author';
+
+export default function Index() {
+    const { name, username, github: urlGithub } = author;
+
+    return (
+        <header className="h-24 flex items-center justify-between px-4">
+            <a
+                href={urlGithub}
+                target="_blank"
+                className="flex gap-4 justify-center items-center cursor-pointer"
+                title={name}
+            >
+                <Logo className="w-8 fill-logo-color animate-spin-logo" />
+                <h2 className="font-space-grotesk font-bold text-2xl text-bold">{username}</h2>
+            </a>
+        </header>
+    );
+}
