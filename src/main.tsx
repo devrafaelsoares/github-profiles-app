30df8c2c1694b3be83ebaf805c0b8b8fce7ff5d4
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-02-02 11:53:34 -0300

    feat: involve the theme module context in every application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { ThemeProvider } from './contexts/ThemeContext.tsx';
 import '@/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
+    <ThemeProvider>
         <App />
-    </React.StrictMode>
+    </ThemeProvider>
 );
